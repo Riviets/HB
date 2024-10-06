23c433fe1ae7d16aae48112cd1bc4d0b64f9f1da
Riviets <vladvladpobuta@gmail.com>
2024-10-06 18:11:52 +0300

    SCRUM-12 Додано маршрути для CRUD-методів для кімнат

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
+use App\Http\Controllers\RoomController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +16,12 @@ Route::get('/hotels/{id}', [HotelController::class, 'show'])->name('hotels.show'
 Route::put('/hotels/{id}', [HotelController::class, 'update'])->name('hotels.update');
 Route::delete('/hotels/{id}', [HotelController::class, 'destroy'])->name('hotels.destroy');
 Route::get('/hotels/{id}/edit', [HotelController::class, 'edit'])->name('hotels.edit');
+
+
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
+Route::get('/rooms/create', [RoomController::class, 'create'])->name('rooms.create');
+Route::post('/rooms', [RoomController::class, 'store'])->name('rooms.store');
+Route::get('/rooms/{id}', [RoomController::class, 'show'])->name('rooms.show');
+Route::get('/rooms/{id}/edit', [RoomController::class, 'edit'])->name('rooms.edit');
+Route::put('/rooms/{id}', [RoomController::class, 'update'])->name('rooms.update');
+Route::delete('/rooms/{id}', [RoomController::class, 'destroy'])->name('rooms.destroy');
