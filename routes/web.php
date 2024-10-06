e3df5950c28956d17953795b5856d976e6c82af7
Riviets <vladvladpobuta@gmail.com>
2024-10-06 16:12:10 +0300

    SCRUM-8 Налаштовано маршрути для методів CRUD для готелів

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HotelController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hotels', [HotelController::class, 'index'])->name('hotels.index');
+Route::post('/hotels', [HotelController::class, 'store'])->name('hotels.store');
+Route::get('/hotels/{id}', [HotelController::class, 'show'])->name('hotels.show');
+Route::put('/hotels/{id}', [HotelController::class, 'update'])->name('hotels.update');
+Route::delete('/hotels/{id}', [HotelController::class, 'destroy'])->name('hotels.destroy');
